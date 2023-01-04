4365700b718d682d7cb78c78704442a42728d25a
binhpct <binhpct@techvn.mti.co.jp>
2023-01-04 11:01:01 +0700

    create prj

---
+++ Survey Demo/Survey Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Survey Demo
+//
+//  Created by Pham Chi Thai Binh(BinhPCT) on 04/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
